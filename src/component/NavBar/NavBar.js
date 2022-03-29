41b7fb612c2b5631fcb07b664686f19fc2ca5103
Janetta Kataoeva <jkattoeva@gmail.com>
2022-03-29 21:08:14 +0600

    Added HomeWork Cards

---
+++ src/component/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import "./NavBar.css"
+
+function NavBar (props) {
+  const linksLeft = ["Link3", "Link4", "Link5"];
+  const linksRight = ["Link1", "Link2"];
+
+const left = linksLeft.map((left) => {
+  return (
+    <li>
+      <a href="#">{left}</a>
+    </li>
+  );
+});
+const right = linksRight.map((right) => {
+  return (
+    <li>
+      <a href="#">{right}</a>
+    </li>
+  );
+});
+
+return(
+  <nav className="NavBar">
+    <ul>
+      <span>{left}</span>
+      <span>{right}</span>
+    </ul>
+  </nav>
+)
+}
+export default NavBar;
